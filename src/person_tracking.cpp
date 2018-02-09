a9ded699230efa7527e5d787b2b8cf125b8a610b
zapv1348 <zacharyapvogel@gmail.com>
2018-02-08 22:20:18 -0800

    NOW WITH 100% MORE CLASSESgit add .git add .git add .git add .git add .git add .!

--- src/person_tracking.cpp
+++ src/person_tracking.cpp
@@ -10,27 +10,69 @@
 using namespace std;
 using namespace cv;
 
-string face_cascade_name = "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml";
-CascadeClassifier face_cascade;
-string window_name = "YO FACE";
 
-void detectAndDisplay(Mat frame){
-    std::vector<Rect> faces;
-    Mat frame_gray;
-    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-    equalizeHist( frame_gray, frame_gray );
-    //-- Detect faces
-    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-    for ( size_t i = 0; i < faces.size(); i++ )
-    {
-        Point topleft (faces[i].x,faces[i].y);
-        Point botright(faces[i].x+faces[i].width,faces[i].y+faces[i].height);
-        rectangle( frame, topleft, botright, Scalar( 255, 0, 255 ), 1, 8, 0 );
-        Mat faceROI = frame_gray( faces[i] );
+//The idea will be to run a thread with this class which just captures frames and returns the current one
+//for now, it is just going to read
+class capture_copy
+{
+private:
+    VideoCapture capture;
+    Mat current_frame;
+public:
+    capture_copy(){
+        capture.open(-1);
+        if ( !capture.isOpened()){
+            //throw exception prolly
+        }
     }
-    //-- Show what you got
-    imshow( window_name, frame );
-}
+    Mat grab_frame(){
+        capture.read(current_frame);
+        return current_frame;   
+    }
+};
+
+class detect_face
+{
+private:
+    Mat frame;
+    Mat grey_frame;
+    std::vector<Rect> face_rectangle;
+    string face_cascade_name = "/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml";
+    CascadeClassifier face_cascade;
+    string window_name = "YO FACE";
+
+public:
+    //currently have to call this for every frame when you want to detect a face.
+    detect_face(){
+        if(!face_cascade.load(face_cascade_name))
+        {
+            //throw an err prolly
+        }
+    }
+    void detect(Mat in_frame){
+        frame=in_frame;
+        cvtColor(frame, grey_frame, COLOR_BGR2GRAY);
+        equalizeHist(grey_frame, grey_frame);
+
+        face_cascade.detectMultiScale(grey_frame, face_rectangle, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30,30));
+        for (size_t i = 0; i< face_rectangle.size(); i++){
+            Point topleft (face_rectangle[i].x,face_rectangle[i].y);
+            Point botright(face_rectangle[i].x+face_rectangle[i].width,face_rectangle[i].y+face_rectangle[i].height);
+            rectangle( frame, topleft, botright, Scalar( 255, 0, 255 ), 1, 8, 0 );
+        }
+    }   
+
+    //just puts it on the screen
+    void display(){
+        imshow(window_name, frame);
+        waitKey(10);
+    }
+
+    //will store the image
+    void capture_image(){
+
+    }
+};
 
 
 #define TEST 1
@@ -38,27 +80,11 @@ void detectAndDisplay(Mat frame){
 
 int main( void )
 {
-    VideoCapture capture;
-    Mat frame;
-    //-- 1. Load the cascades
-    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
-    //-- 2. Read the video stream
-    capture.open( -1 );
-    if ( ! capture.isOpened() ) { printf("--(!)Error opening video capture\n"); return -1; }
-    while ( capture.read(frame) )
-    {
-        if( frame.empty() )
-        {
-            printf(" --(!) No captured frame -- Break!");
-            break;
-        }
-        //-- 3. Apply the classifier to the frame
-        detectAndDisplay( frame );
-        char c = (char)waitKey(10);
-        if( c == 27 ) { break; } // escape
+    capture_copy sup;
+    detect_face foo;
+    while(1){
+        foo.detect(sup.grab_frame());
+        foo.display();
     }
-    return 0;
 }
-
-
 #endif
